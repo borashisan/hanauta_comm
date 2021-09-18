77cf22897ba3a16e46084a384a0a8b10f4eca09b
borashisan <tomochuro731@gmail.com>
2021-09-18 14:04:06 +0900

    -

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    basetitle = "hanauta_comm"
+    if page_title.empty?
+      return basetitle
+    else
+      return basetitle + " | " + page_title 
+    end
+  end
 end
