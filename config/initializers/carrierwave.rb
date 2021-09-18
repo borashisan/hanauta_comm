2925e9508443ab4eb91b69212758a1dcebf41e11
borashisan <tomochuro731@gmail.com>
2021-09-18 19:45:31 +0900

    -

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,8 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'hanauta-comm-bucket' # 作成したバケット名を記述
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: '#{Rails.application.credentials.AWS_ACCESS_KEY}', # 環境変数
-      aws_secret_access_key: '#{Rails.application.credentials.AWS_SECRET_KEY}', # 環境変数
+      aws_access_key_id: Rails.application.credentials.AWS_ACCESS_KEY}, # 環境変数
+      aws_secret_access_key: Rails.application.credentials.AWS_SECRET_KEY, # 環境変数
       region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
       path_style: true
     }
