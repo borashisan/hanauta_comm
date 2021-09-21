e7dbdd2a759de589ee969b78359d64c374d09924
borashisan <tomochuro731@gmail.com>
2021-09-21 15:50:29 +0900

    -

--- config/initializers/aws.rb
+++
@@ -1,7 +0,0 @@
-creds = Aws::Credentials.new(
-  Rails.application.credentials.AWS_ACCESS_KEY,
-  Rails.application.credentials.AWS_SECRET_KEY
-)
-Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: creds, region: 'ap-northeast-1')
-
-config.action_mailer.delivery_method = :aws_sdk
