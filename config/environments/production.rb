04332cb1ae00e88e5a81e8ad2458e0693a030ee6
borashisan <tomochuro731@gmail.com>
2021-09-21 14:27:26 +0900

    -

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'http://hanauta-comm.net/' }
+  config.action_mailer.delivery_method = :aws_sdk
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
