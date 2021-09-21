71aca2cc044b220e9bd896a8bf96868ca1bd1918
borashisan <tomochuro731@gmail.com>
2021-09-21 15:38:41 +0900

    -

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'http://hanauta-comm.net/' }
-  config.action_mailer.delivery_method = :aws_sdk
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.credentials.GMAIL_ADDRESS,
+    password:             Rails.application.credentials.GMAIL_PASSWORD,
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
