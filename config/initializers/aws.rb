674a643c305ae66dc2d83c03794472671a299b55
borashisan <tomochuro731@gmail.com>
2021-09-21 13:51:29 +0900

    -

--- config/initializers/aws.rb
+++
@@ -1,5 +0,0 @@
-ActionMailer::Base.add_delivery_method :ses,
-                                       AWS::SES::Base,
-                                       access_key_id: Rails.application.credentials.AWS_ACCESS_KEY,
-                                       secret_access_key: Rails.application.credentials.AWS_SECRET_KEY,
-                                       server: 'email.ap-northeast-1.amazonaws.com'
